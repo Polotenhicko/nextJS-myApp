40a373530530669f07db78b60907612f6384cd4b
Polo <artem-gribanof@yandex.ru>
2023-07-17 00:08:05 +0300

    hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ReduxWrapper } from "@/component/ReduxWrapper";
+import { ReduxWrapper } from "@/app/component/ReduxWrapper";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
