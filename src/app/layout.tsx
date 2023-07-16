18c61c0fe56f6ad1e15b6e95ead449da8088a6db
Polo <artem-gribanof@yandex.ru>
2023-07-17 00:03:00 +0300

    hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
-"use client";
+import { ReduxWrapper } from "@/component/ReduxWrapper";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Provider } from "react-redux";
-import { store } from "../store/store";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +12,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Provider store={store}>
-      <html lang="ru">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </Provider>
+    <html lang="ru">
+      <body className={inter.className}>
+        <ReduxWrapper>{children}</ReduxWrapper>
+      </body>
+    </html>
   );
 }
