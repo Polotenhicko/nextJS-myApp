c1c8e8a70bcc5ae999f762559c293e6ddf8d9207
Polo <artem-gribanof@yandex.ru>
2023-07-17 11:21:37 +0300

    hotfix

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,28 +1,27 @@
-// "use client";
-import { useSelector, useDispatch } from "react-redux/es/exports";
+"use client";
+import { useSelector, useDispatch } from "react-redux";
 import { TToDo } from "@/store/reducers/todoReducer";
 import { addTodo } from "@/store/actions/todoActions";
 import { useState } from "react";
 
 export default function ToDos() {
-  // const todos: TToDo[] = useSelector(({ todos }) => todos);
-  // const dispatch = useDispatch();
+  const todos: TToDo[] = useSelector(({ todos }) => todos);
+  const dispatch = useDispatch();
 
-  // const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-  // const handleAddTodo = (val: string) => {
-  //   dispatch(addTodo(val));
-  // };
-  // return (
-  //   <div>
-  //     {todos.map((todo) => (
-  //       <div key={todo.id}>{todo.text}</div>
-  //     ))}
-  //     <div>
-  //       <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-  //       <button onClick={() => handleAddTodo(value)}>Отправить</button>
-  //     </div>
-  //   </div>
-  // );
-  return <div>123</div>;
+  const handleAddTodo = (val: string) => {
+    dispatch(addTodo(val));
+  };
+  return (
+    <div>
+      {todos.map((todo) => (
+        <div key={todo.id}>{todo.text}</div>
+      ))}
+      <div>
+        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+        <button onClick={() => handleAddTodo(value)}>Отправить</button>
+      </div>
+    </div>
+  );
 }
